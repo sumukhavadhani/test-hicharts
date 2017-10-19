61661cb665714f954b753d0b92d8e213d5441b56
Sumukh Avadhani <sumukhavadhani@gmail.com>
2017-10-19 12:59:44 +0800

    test-hicharts first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Ng2Highcharts, Ng2Highmaps, Ng2Highstocks} from 'ng2-highcharts';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'High Charts Example';
+	chart: any;
+  constructor() {
+		// this.options = {
+		// 	title : { text : 'simple chart' },
+		// 	series: [{
+		// 			data: [29.9, 71.5, 106.4, 129.2],
+		// 	}]
+		// };
+		this.options = {
+			chart: {
+				type: 'solidgauge'
+			},    
+			title: {text: 'Solid Gauge'},
+			pane: {
+				center: ['50%', '85%'],
+				size: '140%',
+				startAngle: -90,
+				endAngle: 90,
+				background: {
+					backgroundColor:  '#EEE',
+					innerRadius: '60%',
+					outerRadius: '100%',
+					shape: 'arc'
+				}
+			},
+
+			tooltip: {
+					enabled: false
+			},
+
+			// the value axis
+			yAxis: {
+				stops: [
+						[0.1, '#55BF3B'], // green
+						[0.5, '#DDDF0D'], // yellow
+						[0.9, '#DF5353'] // red
+				],
+				lineWidth: 0,
+				minorTickInterval: null,
+				tickAmount: 2,
+				title: {
+						y: -70,
+						text: 'Speed'
+				},
+				labels: {
+						y: 16
+				},
+				min: 0,
+				max: 100
+			},
+			plotOptions: {
+				solidgauge: {
+					dataLabels: {
+						y: 5,
+						borderWidth: 0,
+						useHTML: true
+					}
+				}
+			},
+
+			credits: {
+				enabled: false
+			},
+
+			series: [{
+				name: 'Speed',
+				data: [80],
+				dataLabels: {
+					format: '<div style="text-align:center"><span style="font-size:25px;color: black' +
+										'">{y}</span><br/>' +
+									'<span style="font-size:12px;color:silver">km/h</span></div>'
+				},
+				tooltip: {
+						valueSuffix: ' km/h'
+				}
+			}]
+		};
+	}
+	options: any;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartModule } from 'angular2-highcharts';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+
 
 import { AppComponent } from './app.component';
+
+export declare let require: any;
+
+
+export function highchartsFactory() {
+  const hc = require('highcharts');
+  const dd = require('highcharts/modules/exporting');
+  const sg = require('highcharts/modules/solid-gauge');
+  const hcm = require('highcharts/highcharts-more');  
+  dd(hc);
+  hcm(hc);
+  sg(hc);
+  return hc;
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule
   ],
-  providers: [],
+  providers:[
+    {
+    provide: HighchartsStatic,
+    useFactory: highchartsFactory
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
